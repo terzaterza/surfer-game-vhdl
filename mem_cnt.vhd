1c18250efa23f3ec1eb86e6f402156a855257f5d
terza <lukaterzicbg@gmail.com>
2022-12-08 00:37:28 +0100

    adding architecture of mem controller, figure out how to set read adresses in burst mode

--- mem_cnt.vhd
+++ mem_cnt.vhd
@@ -1,24 +1,87 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 library work;
 use work.surfer_pkg.all;
 
 entity mem_cnt is
     port (
+        clk         : in std_logic;
         add, delete : in std_logic;
         info        : in bomb_info;
-        s_curr      : in game_states;
         
+        burst       : in std_logic;
         core_r_data : in mem_data;
         
+        core_w_en   : out std_logic;
         core_w_data : out mem_data;
         core_w_addr : out mem_addr;
         core_r_addr : out mem_addr;
         
+        disp_w_en   : out std_logic;
         disp_w_data : out mem_data;
         disp_w_addr : out mem_addr;
         
         first       : out bomb_info;
-        size        : out queue_size
+        size        : out q_size_range
     );
 end entity;
+architecture behavioral of mem_cnt is
+    signal queue_head : natural range 0 to q_size_range'high - 1;
+    signal queue_size : q_size_range;
+    
+    signal burst_state : std_logic;
+    signal burst_count : q_size_range;
+begin
+    ADD_DELETE: process(clk) is
+    begin
+        -- add reset for queue_size (and whatever else)
+        if rising_edge(clk) then
+            core_w_en <= '0';
+            if add='1' then
+                queue_size <= queue_size + 1;
+                core_w_addr <= std_logic_vector(to_unsigned(queue_head + queue_size, mem_addr'length));
+                core_w_data <= "00" & info;
+                core_w_en <= '1';
+            elsif delete='1' then
+                -- assert size > 0
+                queue_size <= queue_size - 1;
+                queue_head <= (queue_head + 1) mod q_size_range'high; -- mod only needed for simulation
+            end if;
+        end if;
+    end process;
+    
+    SET_BURST: process(clk) is
+    begin
+        if rising_edge(clk) then
+            if burst='1' then
+                burst_state <= '1';
+                burst_count <= 0;
+                disp_w_en <= '1';
+            elsif burst_count >= queue_size then
+                burst_state <= '0';
+                disp_w_en <= '0';
+            end if;
+        end if;
+    end process;
+    
+    READ_DATA: process(clk) is
+    begin
+        if rising_edge(clk) then
+            if add='1' and queue_size=0 then
+                first <= info;
+            elsif burst='0' then
+                core_r_addr <= std_logic_vector(to_unsigned(queue_head + 1, mem_addr'length));
+                first <= core_r_data(13 downto 0);
+            end if;
+            if burst='1' then
+                disp_w_addr <= std_logic_vector(to_unsigned(burst_count));
+                disp_w_data <= core_r_data;
+                burst_count <= burst_count + 1;
+                -- figure out how and when to set disp_w_en and core_r_addr in burst mode
+            end if;
+        end if;
+    end process;
+    
+    size <= queue_size;
+end architecture;
--- surfer_pkg.vhd
+++ surfer_pkg.vhd
@@ -3,12 +3,11 @@ use ieee.std_logic_1164.all;
 
 package surfer_pkg is
     subtype bomb_info is std_logic_vector(13 downto 0);
-    subtype index is std_logic_vector(2 downto 0);
     
     subtype mem_data is std_logic_vector(15 downto 0);
     subtype mem_addr is std_logic_vector(3 downto 0);
     
-    subtype queue_size is natural range 0 to 8;
+    subtype q_size_range is natural range 0 to 16;
     
     type game_states is (coll_check, add_elem, move, inc_speed, burst_cpy);
 end package surfer_pkg;
