ce470976f72e4e9d7a38607d3412c18a36b4a1f5
terza <lukaterzicbg@gmail.com>
2022-12-11 10:40:12 +0100

    implementing display controller

--- display_controller.vhd
+++ display_controller.vhd
@@ -1,27 +1,59 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 library work;
 use work.surfer_pkg.all;
 
 entity display_controller is
-    port (
-        clk, rst : in std_logic;
-        
-        ref_tick : out std_logic
-    );
-end entity;
+port (
+	clk         : in std_logic;
+	rst         : in std_logic;
+    lane        : in lane_range;
+    xp          : in disp_width_range;
+	yp          : in disp_height_range;
+    size        : in q_size_range; --broj elemenata u memoriji
+    mem_data    : in mem_data; --info o bombi ili novcicu
+    mem_addr    : out mem_addr; -- adresa sa koje citamo info
+    disp_color  : out std_logic_vector(23 downto 0) --boja koju prosledjujemo vga_sync-u
+);
+end display_controller;
 
-architecture testing_only of display_controller is
-    signal count_ref : natural range 0 to 9 := 0;
-begin
-count_process: process(clk, rst) is
-    begin
-        if rst='1' then
-            count_ref <= 0;
-        elsif rising_edge(clk) then
-            count_ref <= (count_ref + 1) mod 10;
-        end if;
-    end process;
+architecture behavioral of display_controller is
+    constant top_edges is array (0 to 2) of integer;
     
-ref_tick <= '1' when count_ref=9 else '0';
-end architecture;
+    signal lookup : q_size_range;
+    signal current_bomb : bomb_info;
+    
+begin
+
+draw: process(lane, xp, yp, size, current_bomb) is
+variable bomb_pos  : natural;
+variable bomb_lane : lane_range;
+begin
+    disp_color <= background_color;
+    if (xp >= c_indent and xp < c_indent + surfer_dim) and
+       (yp >= top_edges(lane) and yp < top_edges(lane) + surfer_dim) then
+        -- draw surfer
+    elsif size > 0 then
+        bomb_pos  := to_integer(signed(current_bomb(13 downto 3)));
+        bomb_lane := to_integer(unsigned(current_bomb(2 downto 1)));
+        if (xp >= bomb_pos and xp < bomb_pos + object_dim) and
+           (yp >= top_edges(bomb_lane) and yp < top_edges(bomb_lane) + object_dim) then
+            -- draw bomb
+        end if;
+    end if;
+end process;
+
+update_lookup: process(clk, rst) is
+begin
+    if rst='1' then
+        lookup <= 0;
+    elsif rising_edge(clk) then
+        
+    end if;
+end process;
+
+current_bomb <= mem_data(13 downto 0);
+mem_addr     <= std_logic_vector(to_unsigned(lookup));
+
+end architecture behavioral;
--- surfer_pkg.vhd
+++ surfer_pkg.vhd
@@ -15,11 +15,20 @@ package surfer_pkg is
     type game_states is (s_coll_check, s_add_elem, s_inc_speed, s_move, s_wait_ref, s_burst_cpy);
     
     constant surfer_dim : natural := 48;
-    constant object_dim : natural := 36;
-    constant c_indent   : natural := 10;
+    constant object_dim : natural := 48;
+--    constant surfer_half_dim : natural := surfer_dim >> 1;
+--    constant object_half_dim : natural := object_dim >> 1;
+    constant c_indent : natural := 10;
+    
+    subtype color is std_logic_vector(23 downto 0);
+    constant background_color : color := x"007777";
+    constant alpha_color      : color := x"ff00ff";
     
     constant spawn_count_min       : natural := 30;
     constant spawn_count_range_len : natural := 150;
     subtype  spawn_count_range is natural range spawn_count_min to spawn_count_min + spawn_count_range_len;
     
+    subtype disp_width_range is natural range 0 to 1023;
+    subtype disp_height_range is natural range 0 to 767;
+        
 end package surfer_pkg;
